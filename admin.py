6d40c9c95865c5d0c9cda2d6fcec085a2ddd824e
phanitejabikkina <phanitejabikkina@gmail.com>
2023-11-18 22:13:12 -0500

    Create admin.py

---
+++ admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import UserProfile, UploadedFile, Transcription, Analysis
+
+admin.site.register(UserProfile)
+admin.site.register(UploadedFile)
+admin.site.register(Transcription)
+admin.site.register(Analysis)
